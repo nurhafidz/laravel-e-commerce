94c1c12187d970d05c57739e09759f9d47d64c4c
nurhafidz muhamad faizal <nurhafidzfaizal03@gmail.com>
2021-03-30 16:48:58 +0700

    hh

--- resources/views/livewire/sellerside.blade.php
+++ resources/views/livewire/sellerside.blade.php
@@ -8,8 +8,8 @@
         <ul class="list-reset flex flex-row md:flex-col text-center md:text-left">
             <li class=" flex-1">
                 <a href="{{url('/dashboard/'.$storename)}}"
-                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500 ">
-                    <svg class="h-8 w-8 text-gray-500 inline-block" width="24" height="24" viewBox="0 0 24 24"
+                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline md:hover:text-red-500 ">
+                    <svg class="h-8 w-8 text-gray-500 hover:text-red-500 inline-block" width="24" height="24" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                         stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" />
@@ -19,13 +19,13 @@
                         <path d="M19 21v-10.15" />
                         <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" /></svg>
                     <span
-                        class="pb-1 md:pb-0 text-xs md:block md:text-xl text-gray-600 md:text-gray-400 block md:inline-block">Toko</span>
+                        class="pb-1 md:pb-0 text-xs md:block md:text-base text-gray-600 hover:text-red-500 md:text-gray-400 block md:inline-block">Toko</span>
                 </a>
             </li>
             <li class="mr-3 flex-1">
                 <a href="{{url('/seller/'.$storename.'/product')}}"
-                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500 ">
-                    <svg class="h-8 w-8 text-gray-500 inline-block" width="24" height="24" viewBox="0 0 24 24"
+                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-red-500 ">
+                    <svg class="h-8 w-8 text-gray-500 hover:text-red-500 inline-block" width="24" height="24" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                         stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" />
@@ -37,8 +37,8 @@
             </li>
             <li class="mr-3 flex-1">
                 <a href="{{url('/seller/'.$storename.'/pesanan')}}"
-                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500  ">
-                    <svg class="h-8 w-8 text-gray-500 inline-block" fill="none" viewBox="0 0 24 24"
+                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-red-500  ">
+                    <svg class="h-8 w-8 text-gray-500 hover:text-red-500 inline-block" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
@@ -49,8 +49,8 @@
             </li>
             <li class="mr-3 flex-1">
                 <a href="{{url('seller/'.$storename.'/saldo')}}"
-                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500  ">
-                    <svg class="h-8 w-8 text-gray-500 inline-block" fill="none" viewBox="0 0 24 24"
+                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-red-500  ">
+                    <svg class="h-8 w-8 text-gray-500 hover:text-red-500 inline-block" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -62,8 +62,8 @@
             </li>
             <li class="mr-3 flex-1">
                 <a href="#"
-                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-pink-500 ">
-                    <svg class="h-8 w-8 text-gray-500 inline-block" fill="none" viewBox="0 0 24 24"
+                    class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-red-500 ">
+                    <svg class="h-8 w-8 text-gray-500 hover:text-red-500 inline-block" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
